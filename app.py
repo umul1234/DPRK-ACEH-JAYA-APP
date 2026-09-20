6495f672999d2797b7c884cab001751f9fc6fbe6
umul1234 <umulpes@gmail.com>
2026-09-21 06:24:01 +0700

    Update greeting from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 # =========================================================
 st.set_page_config(
     page_title="DPRK Aceh Jaya | Portal Informasi Publik",
-    page_icon="🏛️",
+    page_icon="https://i.imgur.com/FnS6rer.png",
     layout="wide",
     initial_sidebar_state="collapsed",
 )
