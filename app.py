98c122fa52700af6b7f99bc15d8530c0c75aa92b
umul1234 <umulpes@gmail.com>
2026-09-22 23:33:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ HERO_SLIDES = [
 SAMBUTAN = {
     "nama": "MUSLIADI Z, S.E",
     "jabatan": "Ketua DPRK Aceh Jaya",
-    "foto": "https://imgur.com/mEWIdvc",
+    "foto": "https://i.imgur.com/mEWIdvc.jpg",
     "assalamualaikum": "Assalamu'alaikum Warahmatullahi Wabarakatuh,",
     "pembuka": "Selamat datang di Portal Resmi Dewan Perwakilan Rakyat Kabupaten (DPRK) Aceh Jaya. Portal ini hadir sebagai gerbang informasi dan komunikasi antara DPRK dengan seluruh masyarakat Aceh Jaya.",
     "quote": "Visi kami adalah mewujudkan DPRK yang responsif, transparan, dan akuntabel dalam menjalankan fungsi legislasi, anggaran, dan pengawasan.",
